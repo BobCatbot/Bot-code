f7a6bcb79d9d8cf82a859f39b58ecc6bb8b66d85
BobCat <98361058+BobCatbot@users.noreply.github.com>
2022-02-02 18:07:06 +0000

    Update _selectmenu.py

--- cogs/_selectmenu.py
+++ cogs/_selectmenu.py
@@ -45,23 +45,23 @@ class Dropdown(discord.ui.Select):
         )
 
     async def callback(self, interaction: discord.Interaction):
-        pre = v.bot.get_value(interaction.guild.id, "prefix")
+        pre = "b!"
 
         if self.values[0] == "General":
             em1 = discord.Embed(color=v.hhelp, description=f"""
-            {pre.data}rank | [Usage](https://www.docs.bobcatbot.xyz/general/rank)
-            {pre.data}afk | [Usage](https://www.docs.bobcatbot.xyz/general/afk)
-            {pre.data}invite | [Usage](https://www.docs.bobcatbot.xyz/general/invite)
-            {pre.data}ping | [Usage](https://www.docs.bobcatbot.xyz/general/ping)
-            {pre.data}dead | [Usage](https://www.docs.bobcatbot.xyz/general/dead)
-            {pre.data}diceroll | [Usage](https://www.docs.bobcatbot.xyz/general/diceroll)
-            {pre.data}coinflip | [Usage](https://www.docs.bobcatbot.xyz/general/coinflip)
-            {pre.data}8ball | [Usage](https://www.docs.bobcatbot.xyz/general/8ball)
-            {pre.data}suggest | [Usage](https://www.docs.bobcatbot.xyz/general/suggestion)
-            {pre.data}hug | [Usage](https://www.docs.bobcatbot.xyz/general/hug)
-            {pre.data}howhot | [Usage](https://www.docs.bobcatbot.xyz/general/howhot)
-            {pre.data}lovecalc | [Usage](https://www.docs.bobcatbot.xyz/general/lovecalc)
-            {pre.data}insult | [Usage](https://www.docs.bobcatbot.xyz/general/insult)
+            {pre}rank | [Usage](https://www.docs.bobcatbot.xyz/general/rank)
+            {pre}afk | [Usage](https://www.docs.bobcatbot.xyz/general/afk)
+            {pre}invite | [Usage](https://www.docs.bobcatbot.xyz/general/invite)
+            {pre}ping | [Usage](https://www.docs.bobcatbot.xyz/general/ping)
+            {pre}dead | [Usage](https://www.docs.bobcatbot.xyz/general/dead)
+            {pre}diceroll | [Usage](https://www.docs.bobcatbot.xyz/general/diceroll)
+            {pre}coinflip | [Usage](https://www.docs.bobcatbot.xyz/general/coinflip)
+            {pre}8ball | [Usage](https://www.docs.bobcatbot.xyz/general/8ball)
+            {pre}suggest | [Usage](https://www.docs.bobcatbot.xyz/general/suggestion)
+            {pre}hug | [Usage](https://www.docs.bobcatbot.xyz/general/hug)
+            {pre}howhot | [Usage](https://www.docs.bobcatbot.xyz/general/howhot)
+            {pre}lovecalc | [Usage](https://www.docs.bobcatbot.xyz/general/lovecalc)
+            {pre}insult | [Usage](https://www.docs.bobcatbot.xyz/general/insult)
             """)
 
             em1.set_author(icon_url=v.banner, name="General Commands.")
@@ -70,18 +70,18 @@ class Dropdown(discord.ui.Select):
         
         if self.values[0] == "Moderation":
             em = discord.Embed(color=v.hhelp, description=f"""
-            {pre.data}clear | [Usage](https://www.docs.bobcatbot.xyz/moderation/clear)
-            {pre.data}kick | [Usage](https://www.docs.bobcatbot.xyz/moderation/kick)
-            {pre.data}ban | [Usage](https://www.docs.bobcatbot.xyz/moderation/ban)
-            {pre.data}unban | [Usage](https://www.docs.bobcatbot.xyz/moderation/unban)
-            {pre.data}mute | [Usage](https://www.docs.bobcatbot.xyz/moderation/mute)
-            {pre.data}unmute | [Usage](https://www.docs.bobcatbot.xyz/moderation/unmute)
-            {pre.data}slowmode | [Usage](https://www.docs.bobcatbot.xyz/moderation/slowmode)
-            {pre.data}poll | [Usage](https://www.docs.bobcatbot.xyz/moderation/poll)
-            {pre.data}giveaway | [Usage](https://www.docs.bobcatbot.xyz/moderation/giveaway)
-            {pre.data}steal | [Usage](https://www.docs.bobcatbot.xyz/moderation/emoji)
-            {pre.data}lockdown | [Usage](https://www.docs.bobcatbot.xyz/moderation/lockdown)
-            {pre.data}unlock | [Usage](https://www.docs.bobcatbot.xyz/moderation/)
+            {pre}clear | [Usage](https://www.docs.bobcatbot.xyz/moderation/clear)
+            {pre}kick | [Usage](https://www.docs.bobcatbot.xyz/moderation/kick)
+            {pre}ban | [Usage](https://www.docs.bobcatbot.xyz/moderation/ban)
+            {pre}unban | [Usage](https://www.docs.bobcatbot.xyz/moderation/unban)
+            {pre}mute | [Usage](https://www.docs.bobcatbot.xyz/moderation/mute)
+            {pre}unmute | [Usage](https://www.docs.bobcatbot.xyz/moderation/unmute)
+            {pre}slowmode | [Usage](https://www.docs.bobcatbot.xyz/moderation/slowmode)
+            {pre}poll | [Usage](https://www.docs.bobcatbot.xyz/moderation/poll)
+            {pre}giveaway | [Usage](https://www.docs.bobcatbot.xyz/moderation/giveaway)
+            {pre}steal | [Usage](https://www.docs.bobcatbot.xyz/moderation/emoji)
+            {pre}lockdown | [Usage](https://www.docs.bobcatbot.xyz/moderation/lockdown)
+            {pre}unlock | [Usage](https://www.docs.bobcatbot.xyz/moderation/)
             """)
 
             em.set_author(icon_url=v.banner, name="Moderation commands.")
@@ -90,16 +90,16 @@ class Dropdown(discord.ui.Select):
 
         if self.values[0] == "Money":
             em = discord.Embed(color=v.hhelp, description=f"""
-            {pre.data}leaderboard | [Usage](https://www.docs.bobcatbot.xyz/money/leaderboard)
-            {pre.data}shop | [Usage](https://www.docs.bobcatbot.xyz/money/shop)
-            {pre.data}balance | [Usage](https://www.docs.bobcatbot.xyz/money/balance)
-            {pre.data}work | [Usage](https://www.docs.bobcatbot.xyz/money/work)
-            {pre.data}withdraw | [Usage](https://www.docs.bobcatbot.xyz/money/withdraw)
-            {pre.data}deposit | [Usage](https://www.docs.bobcatbot.xyz/money/deposit)
-            {pre.data}send | [Usage](https://www.docs.bobcatbot.xyz/money/send)
-            {pre.data}rob | [Usage](https://www.docs.bobcatbot.xyz/money/rob)
-            {pre.data}buy | [Usage](https://www.docs.bobcatbot.xyz/money/buy)
-            {pre.data}sell | [Usage](https://www.docs.bobcatbot.xyz/money/sell)
+            {pre}leaderboard | [Usage](https://www.docs.bobcatbot.xyz/money/leaderboard)
+            {pre}shop | [Usage](https://www.docs.bobcatbot.xyz/money/shop)
+            {pre}balance | [Usage](https://www.docs.bobcatbot.xyz/money/balance)
+            {pre}work | [Usage](https://www.docs.bobcatbot.xyz/money/work)
+            {pre}withdraw | [Usage](https://www.docs.bobcatbot.xyz/money/withdraw)
+            {pre}deposit | [Usage](https://www.docs.bobcatbot.xyz/money/deposit)
+            {pre}send | [Usage](https://www.docs.bobcatbot.xyz/money/send)
+            {pre}rob | [Usage](https://www.docs.bobcatbot.xyz/money/rob)
+            {pre}buy | [Usage](https://www.docs.bobcatbot.xyz/money/buy)
+            {pre}sell | [Usage](https://www.docs.bobcatbot.xyz/money/sell)
             """)
             em.set_author(icon_url=v.banner, name="Money commands.")
 
@@ -113,10 +113,10 @@ class Dropdown(discord.ui.Select):
             
             __**Server Commands**__
             {pre.data}membercount | [Usage](https://www.docs.bobcatbot.xyz/server/membercount)
-            {pre.data}servericon | [Usage](https://www.docs.bobcatbot.xyz/server/servericon)
-            {pre.data}serverbanner | [Usage](https://www.docs.bobcatbot.xyz/server/serverbanner)
-            {pre.data}serverinfo | [Usage](https://www.docs.bobcatbot.xyz/server/serverinfo)
-            {pre.data}roleinfo | [Usage](https://www.docs.bobcatbot.xyz/server/roleinfo)
+            {pre}servericon | [Usage](https://www.docs.bobcatbot.xyz/server/servericon)
+            {pre}serverbanner | [Usage](https://www.docs.bobcatbot.xyz/server/serverbanner)
+            {pre}serverinfo | [Usage](https://www.docs.bobcatbot.xyz/server/serverinfo)
+            {pre}roleinfo | [Usage](https://www.docs.bobcatbot.xyz/server/roleinfo)
             """)
 
             em.set_author(icon_url=v.banner, name="Utils commands.")
@@ -125,12 +125,12 @@ class Dropdown(discord.ui.Select):
             
         if self.values[0] == "Animal":
             em = discord.Embed(color=v.hhelp, description=f"""
-            {pre.data}cat | [Usage](https://www.docs.bobcatbot.xyz/animal/cat) 
-            {pre.data}dog | [Usage](https://www.docs.bobcatbot.xyz/animal/dog)
-            {pre.data}fox | [Usage](https://www.docs.bobcatbot.xyz/animal/fox)
-            {pre.data}panda | [Usage](https://www.docs.bobcatbot.xyz/animal/panda)
-            {pre.data}bird | [Usage](https://www.docs.bobcatbot.xyz/animal/bird)
-            {pre.data}koala | [Usage](https://www.docs.bobcatbot.xyz/animal/koala)
+            {pre)cat | [Usage](https://www.docs.bobcatbot.xyz/animal/cat) 
+            {pre)dog | [Usage](https://www.docs.bobcatbot.xyz/animal/dog)
+            {pre)fox | [Usage](https://www.docs.bobcatbot.xyz/animal/fox)
+            {pre)panda | [Usage](https://www.docs.bobcatbot.xyz/animal/panda)
+            {pre)bird | [Usage](https://www.docs.bobcatbot.xyz/animal/bird)
+            {pre)koala | [Usage](https://www.docs.bobcatbot.xyz/animal/koala)
             """)
 
             em.set_author(icon_url=v.banner, name="Animal commands.")
